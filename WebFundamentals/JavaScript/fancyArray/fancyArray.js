2074e364ac0be5763393a59251a145a74a6150e2
sidneywiredu <sidneywire@yahoo.com>
2017-10-17 01:38:35 -0400

    Removed JS folder

--- WebFundamentals/JavaScript/fancyArray/fancyArray.js
+++
@@ -1,8 +0,0 @@
-function fancyArray(symbol) {
-    var jNames = ["James", "Jill", "Jane", "Jack"];
-
-    for (var i = 0; i < jNames.length; i++) {
-        console.log(i + " " + symbol + " " + jNames[i]);
-    }
-}
-fancyArray("->");
--- WebFundamentals/JavaScript/forAFewBillion/for_few_billion.js
+++
@@ -1,6 +0,0 @@
-var amount = 0.01;
-
-for (var day = 1; day <= 30; day++) {
-    console.log("The reward for day",day,"is", amount);
-    amount *= 2;
-}
